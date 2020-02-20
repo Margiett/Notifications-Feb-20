2c00ac21b084f6721445628f42145edf7c12be10
Margiett Gil <mxg627@icloud.com>
2020-02-20 11:23:39 -0500

    Initial Commit

---
+++ Notifications Feb 20/CreateNotificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateNotificationViewController.swift
+//  Notifications Feb 20
+//
+//  Created by Margiett Gil on 2/20/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class CreateNotificationViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+
+}
---
+++ Notifications Feb 20/NotificationsViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Notifications Feb 20
+//
+//  Created by Margiett Gil on 2/20/20.
+//  Copyright © 2020 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class NotificationsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    //data for tableview
+    private var notification = [String]()
+    // this is a sigleton
+    private let center = UNUserNotificationCenter.current()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        checkForNotificationAuthorization()
+        requestNotificationPermission()
+    }
+     //MARK: asking user permission for notification
+    private func checkForNotificationAuthorization(){
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                print("app is authorized for notifications")
+            } else {
+                self.requestNotificationPermission()
+            }
+            
+        }
+        
+    }
+   // We need permission for alert and sound and have to ask permission for each
+    private func requestNotificationPermission(){
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if let error = error {
+                print("error requesting authorization: \(error)")
+                return
+            }
+            if granted {
+                print("access was granted")
+                
+            } else {
+                print("access denied")
+            }
+        }
+        
+    }
+
+
+}
+
+extension NotificationsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // notification.count 
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "notificationCell", for: indexPath)
+        return cell
+    }
+}
